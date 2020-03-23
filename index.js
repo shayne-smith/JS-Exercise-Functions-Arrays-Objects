e25b8389e20dbb95b4cc62ffee02974b264f3685
seanmx96 <seanmx96@users.noreply.github.com>
2020-03-23 14:15:49 -0500

    Completed appleIndex(array) function

--- index.js
+++ index.js
@@ -147,8 +147,13 @@ function getName(NameObject) {
  * passing in [ 'orange', 'grape', 'apple', 'banana', 'mango' ] as the argument,
  * the returned value should be: 2.
 */
-function appleIndex(/* code here */) {
+function appleIndex(array) {
   /* code here */
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'apple') {
+      return i;
+    }
+  }
 }
 
 /**
